e0d44a89ee04e91cf9572b11cbef894b607640da
hariomphogat <hariom9728034346@gmail.com>
2023-10-05 23:24:56 +0530

    add delete button for threads

--- components/cards/ThreadCard.tsx
+++ components/cards/ThreadCard.tsx
@@ -1,125 +1,157 @@
 import { formatDateString } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import DeletePost from "./DeletePost";
 
 interface Props {
+  id: string;
+  currentUserId: string;
+  parentId: string | null;
+  content: string;
+  author: {
+    name: string;
+    image: string;
     id: string;
-    currentUserId: string;
-    parentId: string | null;
-    content: string;
-    author:{
-        name: string;
-        image: string;
-        id: string;
-    }
-    community:{
-        id: string;
-        name: string;
-        image: string;
-    } | null;
-    createdAt:string;
-    comments:{
-        author: {
-            image: string;
-        }
-    }[]
-    isComment?: boolean;
-    renderCommunityDetails? : boolean;
-
+  };
+  community: {
+    id: string;
+    name: string;
+    image: string;
+  } | null;
+  createdAt: string;
+  comments: {
+    author: {
+      image: string;
+    };
+  }[];
+  isComment?: boolean;
+  renderCommunityDetails?: boolean;
+  path: string;
 }
-
 
 const ThreadCard = ({
-    
-    id,
-    currentUserId,
-    parentId,
-    content,
-    author,
-    community,
-    createdAt,
-    comments,
-    isComment,
-    renderCommunityDetails=true,
-
-}: Props) =>{
-
-return (
-    <article className={`flex w-full flex-col rounded-xl 
-    ${isComment ? 'px-0 xs:px-7' : 'bg-dark-2 p-7'}`}>
-       <div className="flex items-start justify-between">
+  id,
+  currentUserId,
+  parentId,
+  content,
+  author,
+  community,
+  createdAt,
+  comments,
+  isComment,
+  renderCommunityDetails = true,
+  path,
+}: Props) => {
+  return (
+    <article
+      className={`flex w-full flex-col rounded-xl 
+    ${isComment ? "px-0 xs:px-7" : "bg-dark-2 p-7"}`}
+    >
+      <div className="flex items-start justify-between">
         <div className="flex w-full flex-1 flex-row gap-4">
-            <div className="flex flex-col items-center">
-                <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
-                    <Image 
-                    src={author.image}
-                    alt="Profile"
-                    fill
-                    className="cursor-pointer rounded-full"
-
-                    />
-                </Link>
-                <div className="thread-card_bar" />
-            </div>
-            <div className="flex flex-col w-full">
-            <Link href={`/profile/${author.id}`} className="w-fit">
-            <h4 className="cursor-pointer text-base-semibold text-light-1">{author.name}</h4>
-            </Link>
-            <p className="mt-2 text-small-regular text-light-2">
-                {content}
-            </p>
-            <div className={`${isComment ? 'mt-3 mb-5' : 'mt-5'} flex flex-col gap-3`}>
-                <div className="flex gap-3.5">
-                <Image src="/assets/heart-gray.svg" alt="heart" width={24} height={24} className="cursor-pointer object-contain"/>
-                <Link href={`/thread/${id}`}>
-                <Image src="/assets/reply.svg" alt="reply" width={24} height={24} className="cursor-pointer object-contain"/>
-                </Link>
-                <Image src="/assets/repost.svg" alt="repost" width={24} height={24} className="cursor-pointer object-contain"/>
-                <Image src="/assets/share.svg" alt="share" width={24} height={24} className="cursor-pointer object-contain"/>
-                </div>
-                
-                {comments.length > 0 && (
-                    <Link href={`/thread/${id}`}>
-                       <p className="mt-1 text-subtle-medium text-gray-1">{comments.length} replies</p>
-                    </Link>
-                )}
-            </div>
-            
-        
-        {/* TODo: Delete a thread */}
-        {/* TODo: Show comment logos */}
-        </div>
-       </div>
-       </div>
-        
-       {!isComment && (!community || !renderCommunityDetails) &&(
-            <p className="mt-5 flex items-center text-subtle-medium text-gray-1">
-                    {formatDateString(createdAt)}
-            </p>
-        )}
-        {!isComment && community && renderCommunityDetails &&(
-            <Link href={`/communities/${community.id}`} className="mt-5 flex items-center">
-                
-                <p className="text-subtle-medium text-gray-1">
-                    {formatDateString(createdAt)} -
-                </p> 
-                <span className="ml-1 w-4 h-4 rounded-full object-fill relative">
-                <Image 
-                src={community.image}
-                alt={community.name}
+          <div className="flex flex-col items-center">
+            <Link href={`/profile/${author.id}`} className="relative h-11 w-11">
+              <Image
+                src={author.image}
+                alt="Profile"
                 fill
-                className=" rounded-full "
-                />
-                </span>
-                 <p className="text-subtle-medium text-gray-1">
-                        {community.name} Community
-                </p>
-               
-                
+                className="cursor-pointer rounded-full"
+              />
             </Link>
+            <div className="thread-card_bar" />
+          </div>
+          <div className="flex flex-col w-full">
+            <Link href={`/profile/${author.id}`} className="w-fit">
+              <h4 className="cursor-pointer text-base-semibold text-light-1">
+                {author.name}
+              </h4>
+            </Link>
+            <p className="mt-2 text-small-regular text-light-2">{content}</p>
+            <div
+              className={`${
+                isComment ? "mt-3 mb-5" : "mt-5"
+              } flex flex-col gap-3`}
+            >
+              <div className="flex gap-3.5">
+                <Image
+                  src="/assets/heart-gray.svg"
+                  alt="heart"
+                  width={24}
+                  height={24}
+                  className="cursor-pointer object-contain"
+                />
+                <Link href={`/thread/${id}`}>
+                  <Image
+                    src="/assets/reply.svg"
+                    alt="reply"
+                    width={24}
+                    height={24}
+                    className="cursor-pointer object-contain"
+                  />
+                </Link>
+                <Image
+                  src="/assets/repost.svg"
+                  alt="repost"
+                  width={24}
+                  height={24}
+                  className="cursor-pointer object-contain"
+                />
+                <Image
+                  src="/assets/share.svg"
+                  alt="share"
+                  width={24}
+                  height={24}
+                  className="cursor-pointer object-contain"
+                />
+              </div>
+
+              {comments.length > 0 && (
+                <Link href={`/thread/${id}`}>
+                  <p className="mt-1 text-subtle-medium text-gray-1">
+                    {comments.length} replies
+                  </p>
+                </Link>
+              )}
+            </div>
+
+            {/* TODo: Show comment logos */}
+          </div>
+        </div>
+        {/* TODo: Delete a thread */}
+
+        {author.id === currentUserId && (
+          <DeletePost threadId={id} path={path} />
         )}
+      </div>
+
+      {!isComment && (!community || !renderCommunityDetails) && (
+        <p className="mt-5 flex items-center text-subtle-medium text-gray-1">
+          {formatDateString(createdAt)}
+        </p>
+      )}
+      {!isComment && community && renderCommunityDetails && (
+        <Link
+          href={`/communities/${community.id}`}
+          className="mt-5 flex items-center"
+        >
+          <p className="text-subtle-medium text-gray-1">
+            {formatDateString(createdAt)} -
+          </p>
+          <span className="ml-1 w-4 h-4 rounded-full object-fill relative">
+            <Image
+              src={community.image}
+              alt={community.name}
+              fill
+              className=" rounded-full "
+            />
+          </span>
+          <p className="text-subtle-medium text-gray-1">
+            {community.name} Community
+          </p>
+        </Link>
+      )}
     </article>
-)
-}
- 
+  );
+};
+
 export default ThreadCard;
--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -1,46 +1,54 @@
-import {fetchUserPosts } from "@/lib/actions/user.actions";
+import { fetchUserPosts } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
 import ThreadCard from "../cards/ThreadCard";
 import { fetchCommunityPosts } from "@/lib/actions/community.actions";
 
-interface Props{
-    currentUserId:string;
-    accountId:string;
-    accountType:string;
+interface Props {
+  currentUserId: string;
+  accountId: string;
+  accountType: string;
+  path: string;
 }
-const ThreadsTab = async({currentUserId,
-    accountId, accountType}:Props) =>{
-        let result:any;
-        if(accountType === 'Community'){
-            result = await fetchCommunityPosts(accountId);
-           
-        } else{
-            result = await fetchUserPosts(accountId);
-        }
-       
-        if(!result) redirect('/');
-return(
+const ThreadsTab = async ({
+  currentUserId,
+  accountId,
+  accountType,
+  path,
+}: Props) => {
+  let result: any;
+  if (accountType === "Community") {
+    result = await fetchCommunityPosts(accountId);
+  } else {
+    result = await fetchUserPosts(accountId);
+  }
+
+  if (!result) redirect("/");
+  return (
     <section className="mt-9 flex flex-col gap-10">
-       {result.threads.map((thread:any) => (
+      {result.threads.map((thread: any) => (
         <ThreadCard
-        key={thread._id}
-        id={thread._id}
-        currentUserId={currentUserId || ""}
-        parentId={thread.parentId}
-        content={thread.text}
-        author={
-            accountType === 'User'
-            ? {name: result.name, image:result.image, id:result.id}
-            : {name: thread.author.name, image: thread.author.image, id:thread.author.id}
-        } 
-        community={thread.community} //todo
-        createdAt={thread.createdAt}
-        comments={thread.children}
-        renderCommunityDetails={false}
+          key={thread._id}
+          id={thread._id}
+          currentUserId={currentUserId || ""}
+          parentId={thread.parentId}
+          content={thread.text}
+          author={
+            accountType === "User"
+              ? { name: result.name, image: result.image, id: result.id }
+              : {
+                  name: thread.author.name,
+                  image: thread.author.image,
+                  id: thread.author.id,
+                }
+          }
+          community={thread.community} //todo
+          createdAt={thread.createdAt}
+          comments={thread.children}
+          renderCommunityDetails={false}
+          path={path}
         />
-        
-       ))}
+      ))}
     </section>
-)
-}
+  );
+};
 export default ThreadsTab;
